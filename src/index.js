e7c2625b802ce627ad35fe8169bca2c91577f351
Bamsi <SadBamsi@mail.ru>
2020-09-30 19:05:49 +0300

    dev: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracket = {};
+    let ArrOfStr = str.split('');
+    for(let item of bracketsConfig) {
+        bracket[item[0]] = item[1];
+    }
+    let result = []
+    for (let item of ArrOfStr) {
+      bracket[result[result.length - 1]] == item ? result.pop() : result.push(item)  
+    }
+    return result.length == 0;
+  }
